d0a96e842106eba768350b3577f66dfaf8355934
Hazel-1212 <achievegoal9@gmail.com>
2025-04-10 19:20:03 +0800

    Update full_adder

--- full_adder/fa_src.v
+++ full_adder/fa_src.v
@@ -1,3 +1,49 @@
+module top_adder(
+    input [3:0]a,
+    input [3:0]b,
+    input cin,
+    output [3:0]sum,
+    output cout
+);
+
+wire s1, s2, s3, s4;
+wire c1, c2, c3, c4;
+
+full_adder f1(
+    .a(a[0]),
+    .b(b[0]),
+    .cin(cin),
+    .sum(s1),
+    .cout(c1)
+);
+full_adder f2(
+    .a(a[1]),
+    .b(b[1]),
+    .cin(c1),
+    .sum(s2),
+    .cout(c2)
+);
+full_adder f3(
+    .a(a[2]),
+    .b(b[2]),
+    .cin(c2),
+    .sum(s3),
+    .cout(c3)
+
+);
+full_adder f4(
+    .a(a[3]),
+    .b(b[3]),
+    .cin(c3),
+    .sum(s4),
+    .cout(c4)
+);
+
+assign cout = c4;
+assign sum = {s4, s3, s2, s1};
+
+endmodule
+
 module half_adder(
     input a,
     input b,
--- full_adder/tb_fa.v
+++ full_adder/tb_fa.v
@@ -1,26 +1,26 @@
-`timescale 1ns / 1ps
-
-module tb_full_adder();
-reg a,b,cin;
-wire sum,cout;
-
-full_adder uut(
-    .a(a),
-    .b(b),
-    .cin(cin),
-    .sum(sum),
-    .cout(cout)
+module tb_top_adder();
+reg [3:0] a;
+reg [3:0] b;
+reg cin;
+wire [3:0] sum;
+wire cout;
+top_adder uut(
+    .a      (a),
+    .b      (b),
+    .cin    (cin),
+    .sum    (sum),
+    .cout   (cout)
 );
-
-initial 
-   begin
-    #5 {cin,b,a}=3'b000;
-    #5 {cin,b,a}=3'b001;
-    #5 {cin,b,a}=3'b010;
-    #5 {cin,b,a}=3'b011;
-    #5 {cin,b,a}=3'b100;
-    #5 {cin,b,a}=3'b101;
-    #5 {cin,b,a}=3'b110;
-    #5 {cin,b,a}=3'b111;
+integer i;
+integer j;
+initial begin
+  for (i = 0; i < 2; i = i + 1) begin
+        cin=i;
+       for (j = 0; j < 256; j = j + 1) begin
+           #10 {a, b} = j[7:0];
+           #10;
+       end
    end
+end
 endmodule
+
